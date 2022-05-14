7ca975f09941eb4a588198fd499d0368552d6428
thcheetah777 <thcheetah777@gmail.com>
2022-05-13 17:16:58 -0700

    Add some stuff

--- sketch.js
+++ sketch.js
@@ -1,11 +1,15 @@
 // Wandering Circles
-const colors = [[255, 0, 0, 150], [0, 0, 255, 150], [0, 255, 0, 150], [255, 255, 0, 150], [0, 255, 255, 150], [255, 255, 255, 255], [50, 50, 50, 255]];
+const colors = [[255, 0, 0, 127], [0, 0, 255, 127], [0, 255, 0, 127], [255, 255, 0, 127], [0, 255, 255, 127], [255, 255, 255, 127], [50, 50, 50, 127]];
 let t = [];
 let x;
 let y;
+let circleSize = 0;
+let mode = "colors";
+let modes = ["colors", "whiteStroke", "blackStroke"];
+let modeIndex = 0;
 
 // Create circle
-function createCircle(color, a, tele, num1, num2) {
+function createCircle(color, tele, num1, num2) {
   x = width * noise(tele + num1);
   y = height * noise(tele + num2);
 
@@ -16,7 +20,7 @@ function createCircle(color, a, tele, num1, num2) {
   t.push(0);
 
   // Draw an ellipse with the variables for x and y passed in
-  circle(x, y, 50);
+  circle(x, y, circleSize);
 }
 
 // Setup
@@ -31,14 +35,27 @@ function setup() {
 // Draw
 function draw() {
   // Reset background
-  background(0, 16);
+  background(0, 18);
 
   // Settings
-  noStroke();
+  switch (mode) {
+    case "colors":
+      noStroke();
+      break;
+    case "blackStroke":
+      stroke(0);
+      break;
+    case "whiteStroke":
+      stroke(255);
+      break;
+  }
+
+  // Increment circle size
+  circleSize += 0.1;
 
   // Create circle
   for (var i = 0; i < colors.length; i++) {
-    createCircle(colors[i], 150, t[i], 100, 200);
+    createCircle(colors[i], t[i], 100, 200);
   }
 
   // Increment the t variable to control the noise amount
@@ -53,4 +70,13 @@ function draw() {
   let noise3 = 15;
   t[t.length - 1] += noise2;
   t[t.length - 2] += noise3;
+}
+
+// Input
+function mousePressed() {
+  modeIndex++;
+  if (modeIndex >= modes.length) {
+    modeIndex = 0;
+  }
+  mode = modes[modeIndex];
 }
